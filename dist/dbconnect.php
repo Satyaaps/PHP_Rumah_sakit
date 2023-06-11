684934ae1b9caea1458d43ed93d14da703492661
Natania Setyawati <natania.nn64@gmail.com>
2023-06-11 19:35:33 +0800

    benerin dbconnect

--- dist/dbconnect.php
+++ dist/dbconnect.php
@@ -1,16 +1,8 @@
 <?php
-const MongoClient = require('mongodb').MongoClient;
-
-const uri = 'mongodb://localhost:27017'; // URI koneksi MongoDB
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Menghubungkan ke MongoDB
-client.connect((err) => {
-  if (err) {
-    console.error('Koneksi gagal: ', err);
-    return;
-  }
-  console.log('Terhubung ke MongoDB');
-
-});
+    require "../vendor/autoload.php";
+    use MongoDB\Client;
+    $client = new MongoDB\Client;
+    $database = $client->rumah_sakit;
+    //$collection = $database->selectCollection("obat");
+    // $document = $collection->find();
 ?>
