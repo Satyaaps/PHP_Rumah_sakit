23f960b331574cf78e22c852784e978edc1fe3db
satyaaps <satyaariya71@gmail.com>
2023-06-19 18:09:50 +0800

    update adminlistdatamedis

--- dist/adminlistdatamedis.php
+++ dist/adminlistdatamedis.php
@@ -51,6 +51,7 @@ if (isset($_GET['search'])) {
                 <a href="adminlistdatapasien.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">List User</a>
                 <a href="adminlistdatamedis.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">List Medis</a>
                 <a href="adminpembelian.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">List Pembelian Obat</a>
+                <a href="report.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Report</a>
         </div>
     </div>
     <!-- Page content wrapper-->
