15fd5bfce58f81729c0bb989aef9bb1fca28bb26
satyaaps <satyaariya71@gmail.com>
2023-06-19 11:24:20 +0800

    update tambahdataobat

--- dist/tambahdataobat.php
+++ dist/tambahdataobat.php
@@ -13,6 +13,11 @@
     <link href="css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="css/bootstrap.css">
     <script src="js/jquery-3.4.1.min.js"></script>
+    <style>
+        .form-item {
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
     <div class="d-flex" id="wrapper">
@@ -20,7 +25,7 @@
         <div class="border-end bg-white" id="sidebar-wrapper">
             <div class="sidebar-heading border-bottom bg-light">HaiMedic</div>
             <div class="list-group list-group-flush">
-            <a href="admindashboard.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard Admin</a>
+                <a href="admindashboard.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard Admin</a>
                 <a href="adminlistobat.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">List Obat</a>
                 <a href="adminlistdatapasien.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">List User</a>
                 <a href="adminlistdatamedis.php" class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">List Medis</a>
@@ -48,11 +53,6 @@
             <div class="container-fluid">
                 <form action="" method="POST" class="form-item">
                     <div class="form-group">
-                        <label for="ido"> ID OBAT </label>
-                        <input type="text" name="ido" class="form-control col-md-9" placeholder="Masukkan id obat">
-                    </div>
-
-                    <div class="form-group">
                         <label for="nama_obat"> NAMA OBAT </label>
                         <input type="text" name="nama_obat" class="form-control col-md-9" placeholder="Masukkan nama obat">
                     </div>
@@ -75,9 +75,9 @@
                     <div class="form-group">
                         <label for="jenis_obat">JENIS OBAT</label>
                         <select name="jenis_obat" class="form-control col-md-9">
-                            <option value="Tablet" <?php if (isset($data['jenis_obat']) && $data['jenis_obat'] == 'Tablet') echo 'selected'; ?>>Tablet</option>
-                            <option value="Kapsul" <?php if (isset($data['jenis_obat']) && $data['jenis_obat'] == 'Kapsul') echo 'selected'; ?>>Kapsul</option>
-                            <option value="Sirup" <?php if (isset($data['jenis_obat']) && $data['jenis_obat'] == 'Sirup') echo 'selected'; ?>>Sirup</option>
+                            <option value="Tablet" selected>Tablet</option>
+                            <option value="Kapsul">Kapsul</option>
+                            <option value="Sirup">Sirup</option>
                         </select>
                     </div>
 
@@ -98,16 +98,23 @@
 <?php
 require "dbconnect.php";
 $collection = $database->selectCollection("obat");
-$tblmhs = $collection->find();
+
 if (isset($_POST['simpan'])) {
-    $insertOneResult = $collection->insertOne([
-        'ido' => $_POST['ido'],
-        'nama_obat' => $_POST['nama_obat'],
-        'stok' => $_POST['stok'],
-        'harga_jual' => $_POST['harga_jual'],
-        'harga_beli' => $_POST['harga_beli'],
-        'jenis_obat' => $_POST['jenis_obat'],
-    ]);
+    $nama_obat = $_POST['nama_obat'];
+    $stok = $_POST['stok'];
+    $harga_jual = $_POST['harga_jual'];
+    $harga_beli = $_POST['harga_beli'];
+    $jenis_obat = $_POST['jenis_obat'];
+
+    $document = [
+        'nama_obat' => $nama_obat,
+        'stok' => $stok,
+        'harga_jual' => $harga_jual,
+        'harga_beli' => $harga_beli,
+        'jenis_obat' => $jenis_obat,
+    ];
+
+    $insertOneResult = $collection->insertOne($document);
 
     echo "<div align='center'><h5> Silahkan Tunggu, Data Sedang Disimpan....</h5></div>";
     echo "<meta http-equiv='refresh' content='1;url=http://localhost/apaja/dist/adminlistobat.php'>";
