c070a4c0b95274edbadffd0c56e682a1cc5cac13
satyaaps <satyaariya71@gmail.com>
2023-06-19 18:11:46 +0800

    update register

--- dist/register.php
+++ dist/register.php
@@ -26,6 +26,8 @@
       background-color: #fff;
       border-radius: 5px;
       box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+      height: 400px;
+      overflow-y: auto;
     }
     
     .register-container h2 {
@@ -70,31 +72,41 @@
       cursor: pointer;
       font-size: 16px; 
     }
+    .register-container input[type="date"] {
+    display: flex;
+    width: 100%;
+    padding: 12px;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    margin-left: -10px;
+    font-size: 16px;
+
+    }
   </style>
 </head>
 <body>
   <div class="register-container">
     <h2>Register</h2>
     <form action="register.php" method="POST">
-      <input type="text" name="idu" placeholder="ID User" required>
-      <input type="text" name="username" placeholder="Username" required>
-      <input type="password" name="password" placeholder="Password" required>
-      <input type="text" name="nama_pasien" placeholder="Nama Pasien" required>
-      <input type="text" name="no_telp" placeholder="No Telp" required>
-      <input type="number" name="umur" placeholder="Umur" required>
-      <div class="select-container">
-        <select name="jenis_kelamin" required>
+      <label for="username">Username:</label>
+      <input type="text" name="username" required>
+      <label for="password">Password:</label>
+      <input type="password" name="password" required>
+      <label for="nama_pasien">Nama Pasien:</label>
+      <input type="text" name="nama_pasien" required>
+      <label for="no_telp">No.Telepon:</label>
+      <input type="text" name="no_telp" required>
+      <label for="tanggal_lahir">Tanggal Lahir:</label>
+      <input type="date" name="tanggal_lahir" required>
+      <label for="tanggal_lahir">Pilih Jenis Kelamin:</label>
+      <select name="jenis_kelamin" required>
           <option value="" disabled selected>Pilih Jenis Kelamin</option>
           <option value="Laki-laki">Laki-laki</option>
           <option value="Perempuan">Perempuan</option>
-        </select>
-        <select name="status" required>
-          <option value="" disabled selected>Pilih Status</option>
-          <option value="admin">Admin</option>
-          <option value="member">Member</option>
-        </select>
-      </div>
-      <input type="text" name="penyakit_bawaan" placeholder="Penyakit Bawaan">
+      </select>
+      <label for="penyakit_bawaan">Penyakit Bawaan:</label>
+      <input type="text" name="penyakit_bawaan" >
       <div class="btn-container">
         <input type="submit" value="Register">
       </div>
@@ -113,27 +125,28 @@ $collection = $database->selectCollection("user");
 $client = new MongoDB\Client("mongodb://localhost:27017");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $idu = $_POST['idu'];
     $username = $_POST['username'];
     $password = $_POST['password'];
     $nama_pasien = $_POST['nama_pasien'];
     $no_telp = $_POST['no_telp'];
+    $tanggal_lahir = $_POST['tanggal_lahir'];
     $jenis_kelamin = $_POST['jenis_kelamin'];
-    $umur = $_POST['umur'];
     $penyakit_bawaan = $_POST['penyakit_bawaan'];
-    $status = $_POST['status'];
+    
+    // Mengatur status menjadi "member"
+    $status = "member";
 
     // Menyimpan data ke koleksi
     $data = [
-        'idu' => $idu,
+ 
         'username' => $username,
         'password' => $password,
         'nama_pasien' => $nama_pasien,
         'no_telp' => $no_telp,
+        'tanggal_lahir' => $tanggal_lahir,
         'jenis_kelamin' => $jenis_kelamin,
-        'umur' => $umur,
         'penyakit_bawaan' => $penyakit_bawaan,
-        'status' => $status
+        'status' => $status // Menambahkan status pengguna
     ];
     $result = $collection->insertOne($data);
 
